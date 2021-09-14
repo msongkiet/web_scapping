1a73fb112b5e30caec7cb3895d5bfcd089809f5a
msongkiet <m.songkiet@gmail.com>
2021-09-14 16:35:53 +0700

    Create download_price_history.py

---
+++ download_price_history.py
@@ -0,0 +1,52 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# %%
+df = pd.read_csv('symbol.csv', header=[0], index_col=None)
+lst_symbol = df['symbol'].tolist()
+print(len(lst_symbol))
+
+
+# %%
+driver=webdriver.Chrome(executable_path="C:\\chromedriver_win32\\chromedriver.exe")
+driver.quit()
+
+
+# %%
+class MyDriver(object):
+    def __init__(self, driver, default_timeout=2):
+        self.driver = driver
+        self.timeout = default_timeout
+
+    def find_element(self, locator):
+        return WebDriverWait(self.driver, self.timeout).until(EC.presence_of_element_located(locator))
+
+mydriver = MyDriver(driver)
+
+
+# %%
+mydriver=webdriver.Chrome(executable_path="C:\\chromedriver_win32\\chromedriver.exe")
+mydriver.maximize_window()
+
+for symbol in lst_symbol:
+    url = f'http://performance.morningstar.com/stock/performance-return.action?p=price_history_page&t={symbol}&region=tha&culture=en-US'
+    print(url)
+    mydriver.get(url)
+    mydriver.find_element_by_link_text("10Y").click()
+    mydriver.find_element_by_link_text("Yearly").click()
+    mydriver.find_element_by_class_name('large_button_export').click()
+    print("------------ Downloaded ------------")
+
+print("Completed")
+driver.quit()
+
+
+# %%
+
